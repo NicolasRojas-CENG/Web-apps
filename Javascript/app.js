c1468044d2b905efc2c1db2766fff73e4e48a5c0
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2021-08-03 20:52:06 -0400

    Delete app.js

--- Javascript/app.js
+++
@@ -1,9 +0,0 @@
-const todoInput = document.querySelector(".todo_input");
-const todoButton = document.querySelector(".todo_button");
-const todoList = document.querySelector(".todo_List");
-
-todoButton.addEventListener("click", addTodo);
-
-function addTodo(event) {
-  event.preventDefault();
-}
